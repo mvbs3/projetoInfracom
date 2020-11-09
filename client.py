51227ec8597e0026008222c1c434db9db5a96060
Marcelo Victor <mvbs3@cin.ufpe.br>
2020-11-09 16:40:42 -0300

    Ta funcionando, so estamos ajeitando as pontas

--- client.py
+++ client.py
@@ -3,47 +3,64 @@ import PySimpleGUI as sg
 import _thread
 
 sg.theme('BluePurple')
-
-serverIp = gethostname()
+#serverIp = values['-ID-']
 serverPort = 5555
 cliente = socket(AF_INET, SOCK_STREAM)   #cria um socket tcp cliente
-cliente.connect((serverIp, serverPort))  #se conecta a um  socket tcp servidor
 
+#Cria uma caixa onde cada linha faz:
+#Pede username
+#pede IP do server
+#botoes de OK e Cancelar
 event, values = sg.Window('Login',
-                  [[sg.T('Digite seu apelido'), sg.In(key='-ID-')],
+                  [[sg.T('Digite seu apelido:   '), sg.In(key='-ID-')],
+                  [sg.T('Digite IP do servidor:'), sg.In(key='-IP-')],
                   [sg.B('OK', bind_return_key=True), sg.B('Cancel') ]]).read(close=True)
-
+#caso vc clique no botao de sair, vc fecha o programa
+if (event == "Cancel"): 
+    quit()
+#pega o apelido e o ip q vc inseriu
 apelido = values['-ID-']
-
+serverIp = values['-IP-']
 mensagem = ""
-#apelido = input("Escolha seu apelido: ")
+
+#Caso o Username que vc insira tenha Espacos em branco, vc sera obrigado a colocar um nome que nao tenha espacos em branco
 while ' ' in apelido:
     event, values = sg.Window('Login',
-                  [[sg.T('Digite seu apelido sem espaco', key='-TEXTO-'), sg.In(key='-ID-')],
+                  [[sg.T('Digite seu apelido sem espaço', key='-TEXTO-'), sg.In(key='-ID-')],
                   [sg.B('OK'), sg.B('Cancel') ]]).read(close=True)
     apelido = values['-ID-']
-    #print("escolha um apelido sem espaco")
-    #apelido = input("Escolha seu apelido: ")
+    if (event == "Cancel"):
+        quit()
 
-layout = [[sg.Text("Usuário: " + apelido)],
-          [sg.Output(size=(50,10), key='-OUTPUT-')],
+#AQUI VC VAI SE CONECTAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+cliente.connect((serverIp, serverPort))  #se conecta a um  socket tcp servidor
+
+#Define o layout do chat linhas sao:
+#Usuario: apelido
+#Define uma caixa de texto de saida, mostra todas as mensagens do chat
+#Um input para vc escrever suas mensagens
+#botoes de Enviar, limpar tela e Sair
+layout = [[sg.Text("Usuario: " + apelido)],
+          [sg.Output(size=(100,20), key='-OUTPUT-')],
           [sg.Input(key='-MSG-')],
           [sg.Button('Enviar', bind_return_key=True), sg.Button('Clear'), sg.Button('Sair')]]
+#Inicia o chat
+window = sg.Window('Chat', layout)
 
-window = sg.Window('Chat Maravigold', layout)
-
+#RECEBE AAS MENSAGENS E PRINTA AS MENSAGENS NA TELA
 def recebeMensagens():  
     global apelido                                    #Funcao que recebe mensagens do servidor e printa elas
     while (1):
         mensagem = cliente.recv(1024).decode('ascii')   #Tenta receber uma mensagem do servidor(trava o programa aqui)
         if mensagem == 'close':                       #encerrar a conexao quando receber do server o sinal e sair da thread
-            print("saindo desse chat maravigold")
+            print("Saindo desse chat")
             cliente.close()  
             _thread.exit()
             break
         else:
             print(mensagem)
 
+#FUNCAO QUE SERVE PARA CHECAR SE O APELIDO Q VC ESCOLHEU JA FOI ESCOLHIDO
 while mensagem != 'ok':
     mensagem = cliente.recv(1024).decode('ascii')   #Tenta receber uma mensagem do servidor(trava o programa aqui)
     if mensagem == 'Apelido':
@@ -53,19 +70,19 @@ while mensagem != 'ok':
                   [[sg.T('Apelido ja escolhido, escolha outro.'), sg.In(key='-ID-')],
                   [sg.B('OK'), sg.B('Cancel') ]]).read(close=True)
         apelido = values['-ID-']
-        #apelido = input("Apelido ja escolhido, escolha outro: ") 
         cliente.send(apelido.encode('ascii'))
 
 recebe_thread = _thread.start_new_thread(recebeMensagens, ())   #inicia thread de recebimento de mensagem
 
+#LOOP que envia as suas entradas para o servidor
 while True:  # Event Loop
-    event, values = window.read()
+    event, values = window.read() #lendo entrada
+    window['-MSG-'].update("") 
     mensagem = values['-MSG-']
     if event == 'Clear':
         window['-OUTPUT-'].update("")
     if mensagem[0:9] == "send -all" or (event == 'Enviar' and  mensagem[0:9] == "send -all"):
         cliente.send(mensagem.encode('ascii'))
-        #print(mensagem[10:])
         window['-MSG-'].update("")
     if event == sg.WIN_CLOSED or mensagem == 'bye' or event == 'Sair':
         mensagem = 'bye'
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from socket import *
 import _thread
+from datetime import datetime
 
-serverName = '192.168.0.100' #local host
+serverName = "127.0.0.1" #local host
 serverPort = 5555  
 
 serverSocket = socket(AF_INET, SOCK_STREAM) #CRIA SOCKET TCP DO SERVER
@@ -14,12 +15,16 @@ enderecos = []
 
 def formataMensagem(cliente, mensagem): #FORMATA A MENSAGEM PARA DIZER O IP,PORTA, E APELIDO E DPS MENSAGEM
     index = clientes.index(cliente)
-    mensagemFinal = str(enderecos[index][0]) + ":" + str(enderecos[index][1]) + "\~" + str(apelidos[index]) + ": " + mensagem
+    data_e_hora_atuais = datetime.now()
+    data_e_hora_em_texto = data_e_hora_atuais.strftime(" %Hh%M %d/%m/%Y")
+    mensagemFinal = str(enderecos[index][0]) + ":" + str(enderecos[index][1]) + "/~" + str(apelidos[index]) + ": " + mensagem + data_e_hora_em_texto
     return mensagemFinal
 
 def formataMensagemPrivada(cliente, mensagem): #FORMATA A MENSAGEM PARA DIZER O IP,PORTA, E APELIDO E DPS MENSAGEM
     index = clientes.index(cliente)
-    mensagemFinal = str(enderecos[index][0]) + ":" + str(enderecos[index][1]) + "\~" + str(apelidos[index]) + " (PM)" + ": " + mensagem
+    data_e_hora_atuais = datetime.now()
+    data_e_hora_em_texto = data_e_hora_atuais.strftime(" %Hh%M %d/%m/%Y")
+    mensagemFinal = str(enderecos[index][0]) + ":" + str(enderecos[index][1]) + "/~" + str(apelidos[index]) + " (PM)" + ": " + mensagem + data_e_hora_em_texto
     return mensagemFinal
 
 def formataLista(apelidos): #enviar lista de users como uma string so
@@ -40,13 +45,16 @@ def handle(cliente):                                        #Funcao que fica ouv
             mensagem = cliente.recv(1024)               #Tenta receber mensagem do client
             mensagem = mensagem.decode('ascii')
         except:
-            print("errooou")
+            print("Cliente fechou a janela")
             index = clientes.index(cliente)
             apelido = apelidos[index]                   
             endereco = enderecos[index]        #envia a todos que um user saiu do chat
             apelidos.remove(apelido)                      #Tira o Apelido do vetor de apelidos
             enderecos.remove(endereco)                  #tira endereço do vetor de endereços
             clientes.remove(cliente)                    #retira o cliente do vetor de clientes
+            desconectado = apelido + " saiu do chat"
+            print(desconectado)
+            broadcast(desconectado.encode('ascii'))
             cliente.close()                             #fecha conexão do cliente
             _thread.exit()
         if mensagem == 'bye':
@@ -69,14 +77,14 @@ def handle(cliente):                                        #Funcao que fica ouv
             mensagem = formataMensagem(cliente, mensagem[10:])
             broadcast(mensagem.encode('ascii'))
         #send -user usuario mensagem
-        #SONIKKU colocar sinalizacao de mensagem privada na mensagem privada
+       
         elif mensagem[0:10] == "send -user":                #envia para o user (que existe) a mensagem privada
             lista = mensagem.split(' ')
             if lista[2] in apelidos:
                 index = apelidos.index(lista[2])
                 meuCliente = clientes[index]
-                cliente.send(formataMensagemPrivada(cliente,"".join(lista[3:])).encode('ascii'))
-                meuCliente.send(formataMensagemPrivada(cliente,"".join(lista[3:])).encode('ascii'))
+                cliente.send(formataMensagemPrivada(cliente," ".join(lista[3:])).encode('ascii'))
+                meuCliente.send(formataMensagemPrivada(cliente," ".join(lista[3:])).encode('ascii'))
             else:                   #caso user nao exista, apenas retorna um erro para o cliente que a solicitou
                 cliente.send("Usuario nao existe".encode('ascii'))
         
@@ -88,22 +96,24 @@ def conexoes():                                         #Funcao que aceita conex
         print(palavra)
 
         cliente.send('Apelido'.encode('ascii'))         #Pergunta ao cliente qual seu Apelido
-        apelido = cliente.recv(1024).decode('ascii')       #recebe o apelido do cliente
-        while apelido in apelidos:
-            cliente.send('Apelido2'.encode('ascii'))         #Pergunta ao cliente qual seu Apelido
-            apelido = cliente.recv(1024).decode('ascii')
-        cliente.send('ok'.encode('ascii')) 
-        apelidos.append(apelido)                        #Salva o apelido na lista de apelidos
-        clientes.append(cliente)                        #Salva cliente na lista de clientes
-        enderecos.append(endereco)
-        
-        menConexao = "O apelido do cliente eh: " + apelido          
-        print(menConexao)                                          #Printa o apelido do cliente recebido no servidor
+        try:
+            apelido = cliente.recv(1024).decode('ascii')       #recebe o apelido do cliente
+            while apelido in apelidos:
+                cliente.send('Apelido2'.encode('ascii'))         #Pergunta ao cliente qual seu Apelido
+                apelido = cliente.recv(1024).decode('ascii')
+            cliente.send('ok'.encode('ascii')) 
+            apelidos.append(apelido)                        #Salva o apelido na lista de apelidos
+            clientes.append(cliente)                        #Salva cliente na lista de clientes
+            enderecos.append(endereco)
+            
+            menConexao = "O apelido do cliente eh: " + apelido          
+            print(menConexao)                                          #Printa o apelido do cliente recebido no servidor
 
-        menConexao = apelido + " entrou no chat"                    
-        broadcast(menConexao.encode('ascii'))                      #Envia para todos os cliente que um novo usuario se conectou
+            menConexao = apelido + " entrou no chat"                    
+            broadcast(menConexao.encode('ascii'))                      #Envia para todos os cliente que um novo usuario se conectou
 
-        _thread.start_new_thread(handle, (cliente,))  #Cria uma thread para ficar ouvindo as mensagens desse cliente                                         #starta a Thread
-
+            _thread.start_new_thread(handle, (cliente,))  #Cria uma thread para ficar ouvindo as mensagens desse cliente                                         #starta a Thread
+        except:
+            print("Tentativa de conexao falhou")
 print("Server iniciado!")
 conexoes()
